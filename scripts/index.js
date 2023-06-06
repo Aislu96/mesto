0ee811f83fd55c71e969e3ab5d908374be557778
Aislu Kharisova <arslanova1996ais@list.ru>
2023-06-06 11:16:06 +0300

    Проект Место. Второе ревью.

---
+++ scripts/index.js
@@ -0,0 +1,37 @@
+const editButton = document.querySelector('.profile__edit-button');
+const nameInput = document.querySelector('.profile__title');
+const jobInput = document.querySelector('.profile__subtitle');
+const popup = document.querySelector('.popup');
+const popupClose = document.querySelector('.popup__close');
+const formName = document.querySelector('.popup__input_type_name');
+const formJob = document.querySelector('.popup__input_type_job');
+const popupEditForm = document.querySelector('.popup__container');
+
+
+// Функция открытия popup
+function editOpenForm() {
+    popup.classList.add('popup_opened');
+    formName.value = nameInput.textContent;
+    formJob.value = jobInput.textContent;
+}
+
+// Функция закрытия popup
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+
+
+// Функция редактирования профиля
+function profileEditing(evt) {
+    evt.preventDefault();
+    nameInput.textContent = formName.value;
+    jobInput.textContent = formJob.value;
+    popup.classList.remove('popup_opened');
+}
+
+
+// Вызов функции
+editButton.addEventListener('click', editOpenForm);
+popupClose.addEventListener('click', closePopup);
+popupEditForm.addEventListener('submit', profileEditing);
+
